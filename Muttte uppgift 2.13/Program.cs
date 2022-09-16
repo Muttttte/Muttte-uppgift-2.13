26cbeb56b251b7d581038ea1c0762f709a01df6a
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:49:09 +0200

    Add project files.

---
+++ Muttte uppgift 2.13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// Mustafa uppgift 2.13 
+
+namespace uppgift_2_13
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Lön för Mutteone");
+            int Mutteone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lön för Muttetvå");
+            int Muttetwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("lön för Muttetre");
+            int Muttetre = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Mutteone" + Mutteone + "kr" + "Muttetwo" + Muttetwo + "kr" + "Muttetre" + Muttetre + "kr");
+
+            Console.WriteLine((Mutteone + Muttetwo + Muttetre) / 3);
+        }
+    }
+}
